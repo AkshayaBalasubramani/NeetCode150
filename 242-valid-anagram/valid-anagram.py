9925e1d2e4ef2268d34d60785c86a28825197e65
Akshaya B <128574846+AkshayaBalasubramani@users.noreply.github.com>
2024-11-19 14:25:37 +0530

    Time: 31 ms (5.39%) | Memory: 16.6 MB (90.20%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,5 +1,26 @@
+# class Solution:
+#     def isAnagram(self, s: str, t: str) -> bool:
+#         if len(s)!=len(t):
+#             return False
+#         return sorted(s)==sorted(t)
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s)!=len(t):
+        d1={}
+        for i in s:
+            if i in d1.keys():
+                d1.update({i:d1.get(i)+1})
+            else:
+                d1[i]=1
+
+        d2={}
+        for i in t:
+            if i in d2.keys():
+                d2.update({i:d2.get(i)+1})
+            else:
+                d2[i]=1
+        if d1==d2:
+            return True
+        else:
             return False
-        return sorted(s)==sorted(t)
+        
+        
