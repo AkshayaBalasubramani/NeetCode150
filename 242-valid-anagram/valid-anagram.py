e58e76dd30b8aa196485a67c17c9350ab7f3414e
Akshaya B <128574846+AkshayaBalasubramani@users.noreply.github.com>
2024-11-19 14:28:06 +0530

    Time: 11 ms (75.44%) | Memory: 16.9 MB (50.86%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -4,23 +4,35 @@
 #             return False
 #         return sorted(s)==sorted(t)
 class Solution:
-    def isAnagram(self, s: str, t: str) -> bool:
-        d1={}
-        for i in s:
-            if i in d1.keys():
-                d1.update({i:d1.get(i)+1})
-            else:
-                d1[i]=1
+#     def isAnagram(self, s: str, t: str) -> bool:
+#         d1={}
+#         for i in s:
+#             if i in d1.keys():
+#                 d1.update({i:d1.get(i)+1})
+#             else:
+#                 d1[i]=1
 
-        d2={}
-        for i in t:
-            if i in d2.keys():
-                d2.update({i:d2.get(i)+1})
-            else:
-                d2[i]=1
-        if d1==d2:
+#         d2={}
+#         for i in t:
+#             if i in d2.keys():
+#                 d2.update({i:d2.get(i)+1})
+#             else:
+#                 d2[i]=1
+#         if d1==d2:
+#             return True
+#         else:
+#             return False
+        def isAnagram(self, s, t):
+            if len(s) != len(t):
+                return False
+        
+            freq = [0] * 26
+            for i in range(len(s)):
+                freq[ord(s[i]) - ord('a')] += 1
+                freq[ord(t[i]) - ord('a')] -= 1
+
+            for i in range(len(freq)):
+                if freq[i] != 0:
+                    return False
+
             return True
-        else:
-            return False
-        
-        
