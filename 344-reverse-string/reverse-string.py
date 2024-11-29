d5784b92219411277ab5090dc58d997490cf7341
Akshaya B <128574846+AkshayaBalasubramani@users.noreply.github.com>
2024-11-29 11:07:10 +0530

    Time: 3 ms (43.42%) | Memory: 29.2 MB (5.26%) - LeetSync

--- 344-reverse-string/reverse-string.py
+++ 344-reverse-string/reverse-string.py
@@ -8,12 +8,19 @@ class Solution:
         #     l+=1
         #     r-=1
             
-        #O(n) stack
-        stack=[]
-        for c in s:
-            stack.append(c)
-        i=0
-        while stack:
-            s[i]=stack.pop()
-            i+=1
+        # #O(n) stack
+        # stack=[]
+        # for c in s:
+        #     stack.append(c)
+        # i=0
+        # while stack:
+        #     s[i]=stack.pop()
+        #     i+=1
+        
+        #O(n) recursive call stack
+        def rev(l,r):
+            if l<r:
+                s[l],s[r]=s[r],s[l]
+                rev(l+1,r-1)
+        rev(0,len(s)-1)
         
